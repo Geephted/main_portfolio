747338695bd4aba061ff420acfe41fd26770a8b0
Gift Joweigha <geephted@gmail.com>
2025-05-16 15:17:06 +0100

    portfolio

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Contact Me
+const emailBtn = (document.querySelector ('.email-btn'));
+const phoneBtn = (document.querySelector('.phone-btn'));
+
+function emailButton() {
+    if (emailBtn.innerHTML === 'Email' ) {emailBtn.innerHTML = 'gift.joweigha@gmail.com' }
+    else {emailBtn.innerHTML = 'Email'}
+};
+
+function phoneButton () {
+    if (phoneBtn.innerHTML === 'Phone Number') { phoneBtn.innerHTML = '+2347034646934'}
+    else {phoneBtn.innerHTML = 'Phone Number'}
+};
+
+
 // Mobile menu toggle
 document.querySelector('.menu-toggle').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('active');
